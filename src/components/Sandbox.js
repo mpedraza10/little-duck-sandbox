773f06c242a9d08b14dabe7aa910f726fb4fe74c
Miguel Pedraza <mikeped469@gmail.com>
2024-05-28 17:58:41 -0600

    Changed env variables

--- src/components/Sandbox.js
+++ src/components/Sandbox.js
@@ -19,7 +19,7 @@ main
 end`
 	);
 	const [result, setResult] = useState("");
-	const api = process.env.SERVER_API || "http://127.0.0.1:5000";
+	const api = process.env.NEXT_PUBLIC_SERVER_API;
 
 	const handleCompile = async () => {
 		// Clear the result
